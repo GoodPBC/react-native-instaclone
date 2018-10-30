685a05aae14b43b0d95e113256f31d53e0d90310
Joseph Pulaski <jpulaski79@gmail.com>
2018-10-30 16:32:49 -0400

    Rende App => Card => AuthorRow => Avatar

--- react-native-instagram-clone/App.js
+++ react-native-instagram-clone/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Constants } from "expo";
-import AuthorRow from "./components/AuthorRow";
+import Card from "./components/Card";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <AuthorRow
+        <Card
           fullname={"First Last"}
           linkText={"Comments"}
           onPressLinkText={() => {
             console.log("Comments Link Clicked");
           }}
+          image={{ uri: "https://unsplash.it/600/600" }}
         />
+        {/* Card.js image prop || 'src={image}' */}
       </View>
     );
   }
