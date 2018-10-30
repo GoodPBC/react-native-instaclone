a8801830d5e0721110019f3b610eb96c6aa33cef
Joseph Pulaski <jpulaski79@gmail.com>
2018-10-30 13:46:54 -0400

    setup props ,propTypes & style for AuthorRow.js

---
+++ react-native-instagram-clone/components/AuthorRow.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import PropTypes from "prop-types";
+import React from "react";
+
+import Avatar from "./Avatar";
+import getAvatarColor from "../utils/getAvatarColor";
+import getInitials from "../utils/getInitials";
+
+const AuthorRow = ({ fullName, linkText, onPressLinkText }) => {
+  // ... write component here
+};
+
+AuthorRow.propTypes = {
+  fullName: PropTypes.string.isRequired,
+  linkText: PropTypes.string.isRequired,
+  onPressLinkText: PropTypes.func.isRequired
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 50,
+    flexDirection: "row",
+    alignContent: "center",
+    paddingHorizontal: 10
+  },
+  text: {
+    marginHorizontal: 6,
+    //use flex: 1 to expand Text to fill any remaining space in the View. Will push TouchableOpacity to right
+    flex: 1
+  }
+});
+
+export default AuthorRow;
