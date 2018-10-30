d29b72ce8d385328cfa225e06438eda24dbda18f
Joseph Pulaski <jpulaski79@gmail.com>
2018-10-30 13:13:27 -0400

    render avatar with PropTypes

--- react-native-instagram-clone/App.js
+++ react-native-instagram-clone/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Avatar from "./components/Avatar";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Avatar
+          initials={"JP"}
+          size={100}
+          backgroundColor={"teal"}
+          borderRadius
+        />
       </View>
     );
   }
@@ -14,8 +20,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
